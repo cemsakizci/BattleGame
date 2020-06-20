de44583a281538b2449d5b164d8862e7bcc7febf
yigitcan9797 <46279104+yigitcan9797@users.noreply.github.com>
2020-06-20 19:56:09 +0300

    User class updated

--- src/business/User.java
+++ src/business/User.java
@@ -4,63 +4,98 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class User {
-	private ArrayList<IVehicle> vehicles;
-	private int sameTypeLimit = 2;
+	private ArrayList<IVehicle> planeList;
+	private ArrayList<IVehicle> shipList;
+	private int sameVehicleLimit = 3; // only 3 ships or planes allowed
+	private int sameTypeLimit = 2; // only 2 same specific type (ex: fighter planes )allowed
 	private Random rand;
 	
 	public User(){
-		vehicles = new ArrayList<IVehicle>();
+		planeList = new ArrayList<IVehicle>();
+		shipList = new ArrayList<IVehicle>();
 		rand = new Random();
 	}
 	
-	public boolean addShip(AbstractVehicleFactory factory, VehicleType type){
+	public boolean addShip(AbstractVehicleFactory factory, VehicleType vehicleType){
+		if(shipList.size() == sameVehicleLimit){
+			return false;
+		}
+		int typeCounter = 0;
+		for(IVehicle ship:shipList){
+			if(ship.getType() == vehicleType)
+				typeCounter++;
+			if(typeCounter == sameTypeLimit){
+				return false;
+			}
+		}
+		IVehicle newShip = factory.createVehicle(vehicleType);
+		shipList.add(newShip);
 		return true;
 	}
 	
 	public boolean addPlane(AbstractVehicleFactory factory, VehicleType vehicleType, EngineType engineType){
-		return true;
-	}
-	
-	public boolean addAddable(AbstractVehicleFactory factory, AddableType type){
-		return true;
-	}
-	
-	private boolean checkLegalAdd(VehicleType type){
+		if(planeList.size() == sameVehicleLimit){
+			return false;
+		}
 		int typeCounter = 0;
-		for(IVehicle vehicle:vehicles){
-			if(type == vehicle.getType()){
+		for(IVehicle plane:planeList){
+			if(plane.getType() == vehicleType)
 				typeCounter++;
-				if(typeCounter == sameTypeLimit){
-					return false;
-				};
+			if(typeCounter == sameTypeLimit){
+				return false;
 			}
 		}
+		IVehicle newPlane = factory.createVehicle(vehicleType, engineType);
+		planeList.add(newPlane);
 		return true;
+	}
+	
+	public boolean addShipAddable(AbstractVehicleFactory factory, AddableType type, int index){
+		IAddable updatedShip = factory.createAddable(shipList.get(index), type);
+		if(updatedShip != null){
+			shipList.set(index, updatedShip);
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean addPlaneAddable(AbstractVehicleFactory factory, AddableType type, int index){
+		IAddable updatedPlane = factory.createAddable(planeList.get(index), type);
+		if(updatedPlane != null){
+			planeList.set(index, updatedPlane);
+			return true;
+		}
+		return false;
 	}
 	
 	public void resetVehicles(){
-		vehicles.clear();
+		shipList.clear();
+		planeList.clear();
 	}
 	
 	public int calculateTotalPoints(){
 		int sum = 0;
-		for(IVehicle vehicle:vehicles){
-			int min = vehicle.getMinPoint();
-			int max = vehicle.getMaxPoint();
+		for(IVehicle ship:shipList){
+			int min = ship.getMinPoint();
+			int max = ship.getMaxPoint();
+			int score = rand.nextInt(max+1) + min;
+			sum += score;
+		}
+		
+		for(IVehicle plane:planeList){
+			int min = plane.getMinPoint();
+			int max = plane.getMaxPoint();
 			int score = rand.nextInt(max+1) + min;
 			sum += score;
 		}
 		return sum;
 	}
 	
-	@Override
-	public String toString(){
-		int index = 1;
-		String output = "";
-		for(IVehicle vehicle:vehicles){
-			output += String.valueOf(index) + ". "; // Item types should be concatenated here
-			index++;
-		}
-		return output;
+	public ArrayList<IVehicle> getShipList(){
+		return shipList;
+	}
+	
+	public ArrayList<IVehicle> getPlaneList(){
+		return planeList;
 	}
 }
